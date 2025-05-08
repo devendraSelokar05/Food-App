26fc60e5130ee307956e616f9c613bf983aa7e55
Devendra Selokar <selokardevendra05@gmail.com>
2025-05-08 19:26:15 +0530

    updated code

--- frontend/src/components/Tags/Tags.js
+++ frontend/src/components/Tags/Tags.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import classes from './tags.module.css';
 const API_URL = process.env.REACT_APP_API_BASE_URL;
+
 export default function Tags({ tags=[], forFoodPage }) {
   if (!Array.isArray(tags)) {
     console.error("Expected 'tags' to be an array, but got:", tags);
